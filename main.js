fc9da5c02f631bfac21edf6de7c0ec335962ae67
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-05-02 23:30:11 +0530

    added api specification

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { router, prisma } = require("./todo");
+const apiSpec = require("./api.json");
 
 const app = express();
 
@@ -7,8 +8,12 @@ app.use(express.json());
 
 app.use("/todos", router);
 
-app.get("/", function (req, res) {
+app.get("/", (_req, res) => {
   return res.send({ message: "Welcome to the todo API!" });
+});
+
+app.get("/api", (_req, res) => {
+  return res.send(apiSpec);
 });
 
 async function main() {
